26a321d124c338de7f64a330e574f5805ee33517
Allan R. Greffin <48775941+AllanGreffin@users.noreply.github.com>
2022-12-01 23:28:43 -0300

    post e update Pets

--- src/app/pet-list/pet-details/pet-details.component.ts
+++ src/app/pet-list/pet-details/pet-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Pet } from 'src/app/models/pet.model';
+import { PetService } from 'src/app/services/pet.service';
 
 @Component({
   selector: 'app-pet-details',
@@ -14,20 +15,29 @@ export class PetDetailsComponent implements OnInit {
   petForm: FormGroup;
 
   constructor(private router: Router,
-    private formBuilder: FormBuilder) {
-     }
+    private formBuilder: FormBuilder,
+    private petService: PetService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.buildForm();
+    let id = this.route.snapshot.paramMap.get('id');
+    if(id){
+      this.petService.GetPetById(id).subscribe((data: any) => {
+        this.pet = data;
+        this.buildForm();
+      });
+    }else{
+      this.buildForm();
+    }
   }
 
-  print(str?: string){
-    this.pet = this.petForm.value;
-    console.log(JSON.stringify(this.pet));
+  back(){
+    this.router.navigate(['/pet-list']);
   }
 
   buildForm(){
     this.petForm = this.formBuilder.group({
+      id: [this.pet.id],
       name: [this.pet.name],
       animalType: [this.pet.animalType],
       breed: [this.pet.breed],
@@ -38,5 +48,20 @@ export class PetDetailsComponent implements OnInit {
       observation: [this.pet.observation],
       imageUrl: [this.pet.imageUrl]
     });
+  }
+
+  save(){
+    this.pet = this.petForm.value;
+    this.pet.userId = JSON.parse(localStorage.getItem("user") as string).id;
+    if(this.pet.id){
+      this.petService.PutPetById(this.pet.id, this.pet).subscribe((data: any) => {
+        this.router.navigate(['/pet-list']);
+      });
+    }else{
+      this.petService.PostPet(this.pet).subscribe((data: any) => {
+        console.log(data);
+        this.router.navigate(['/pet-list']);
+      });
+    }
   }
 }
